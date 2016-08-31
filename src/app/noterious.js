9407c360b8796467dfba22063778cacff5629d1d
George <georgejweiler@gmail.com>
2016-08-31 16:21:04 -0700

    commenting on noterious.js

--- src/app/noterious.js
+++ src/app/noterious.js
@@ -1,6 +1,10 @@
 'use strict';
 
-angular.module('noterious', [
+angular.module('noterious', [ //main container is called "notorious" (top level container) | instantiated on the html tag (or body tag) | wherever the ng-app module is instantiated
+  //do NOT put ng-App on the head tag
+
+  //dependencies are listed here:
+      // these modules are being chosen 'off the menu'
   'ui.router',
   'ngAnimate',
   'firebase',
@@ -9,20 +13,22 @@ angular.module('noterious', [
 ])
   .constant('ENDPOINT_URI', 'https://noterious.firebaseio.com/')
   .config(function ($stateProvider, $urlRouterProvider) {
-    $urlRouterProvider.otherwise('/boards');
+    $urlRouterProvider.otherwise('/boards'); // if no match the state, redirect to default route here
 
-    $stateProvider
+    $stateProvider // all of the different routes:
       .state('login', {
         url:'/login',
         templateUrl: 'app/login/login.tmpl.html',
         controller: 'LoginCtrl',
-        controllerAs: 'login'
+        controllerAs: 'login' //shorthand rename of the controller?
       })
       .state('boards', {
         url:'/boards',
         templateUrl: 'app/boards/boards-mdv.tmpl.html',
         controller: 'BoardsCtrl',
-        controllerAs: 'ctrl',
+        controllerAs: 'ctrl', //shorthand rename of the controller?
+
+        //Data needs to be available for the route (authenticate on firebase before you go to the route):
         resolve: {
           'currentUser': ['Auth', function (Auth) {
             return Auth.$requireAuth();
@@ -33,7 +39,9 @@ angular.module('noterious', [
         url:'/boards/:boardId/notes',
         templateUrl: 'app/notes/notes-mdv.tmpl.html',
         controller: 'NotesCtrl',
-        controllerAs: 'ctrl',
+        controllerAs: 'ctrl', //shorthand rename of the controller?
+
+        //Data needs to be available for the route (authenticate on firebase before you go to the route):
         resolve: {
           'currentUser': ['Auth', function (Auth) {
             return Auth.$requireAuth();
@@ -45,7 +53,7 @@ angular.module('noterious', [
   .run(function ($rootScope, $state) {
     $rootScope.$on('$stateChangeError', function (event, toState, toParams, fromState, fromParams, error) {
       event.preventDefault();
-      if (error === 'AUTH_REQUIRED') {
+      if (error === 'AUTH_REQUIRED') { //if something goes wrong, redirect to login
         $state.go('login');
       }
     });
